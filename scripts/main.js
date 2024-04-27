e18d3c7c7138ae244eb54bb87d26096baefa5552
edvancod <edvan.s.pereira@senai.com.br>
2024-04-27 18:07:46 -0300

    criando o layout do formulario

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,9 @@
 
 document.getElementById('form').addEventListener('submit', function(e){
     e.preventDefault();
-    const usuarioInput = document.getElementById('usuario').value;
-    const senhaInput = document.getElementById('senha').value;
-    const usuario = usuarioInput.value;
-    const senha = senhaInput.value;
+    const usuario = document.getElementById('usuario').value;
+    const senha = document.getElementById('senha').value;
+  
     const msg = document.getElementById('alerta')
 
 if( usuario == 'senai' && senha == 'HRT2024'){
@@ -19,12 +18,4 @@ msg.style.display = 'block';
 }
 
 
-});
-document.getElementById('usuario').addEventListener('focus', function() {
-    // Verifica se o usuário é correto ao ganhar o foco
-    if (this.value === 'senai') {
-        this.style.borderColor = 'green';
-    } else {
-        this.style.borderColor = ''; // Reinicia a cor da borda se não for o usuário correto
-    }
 });
